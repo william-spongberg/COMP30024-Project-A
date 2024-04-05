e6bba7d5f9c58f6c02449a729fa0071a568a4435
XinMoZ <GaoyongleZ@student.unimelb.edu.au>
2024-04-05 18:55:29 +1100

    complete the tetronimos

--- search/tetronimos.py
+++ search/tetronimos.py
@@ -8,10 +8,36 @@ def get_tetronimos() -> list[PlaceAction]:
     """
     tetronimos = [
         PlaceAction(Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(3, 0)),  # I (straight)
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0), Coord(2, 0)), # center shift down 1
+        PlaceAction(Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0)), # center shift down 2
+        PlaceAction(Coord(BOARD_N-3, 0), Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift down 3
+        
         PlaceAction(Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(1, 1)),  # T (T-shape)
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0), Coord(0, 1)), # center shift down 1
+        PlaceAction(Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(BOARD_N-1, 1)), # center shift down 2
+        PlaceAction(Coord(BOARD_N-1,BOARD_N-1), Coord(0,BOARD_N-1), Coord(1,BOARD_N-1), Coord(0, 0)), # center shift down 1 right 1
+        
         PlaceAction(Coord(0, 0), Coord(1, 0), Coord(1, 1), Coord(2, 1)),  # S (S-shape)
-        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 1), Coord(2, 1)),  # J (J-shape)
-        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 0), Coord(2, 0)),  # L (L-shape)
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(0, 1), Coord(1, 1)), # center shift down 1
+        PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, 0)), # center shift down 1 right 1
+        # last case included in rotated tetronimos
+        # PlaceAction(Coord(BOARD_N-2, BOARD_N-1), Coord(BOARD_N-1, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift down 2 right 1
+        
+        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 1), Coord(1, 2)), # Z (Z-shape)
+        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, 0), Coord(1, 1)), # center shift right 1
+        PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(0, 1)), # center shift right 1 down 1
+        # last case included in rotated tetronimos
+        # PlaceAction(Coord(BOARD_N-1, BOARD_N-2), Coord(BOARD_N-1, BOARD_N-1), Coord(0, BOARD_N-1), Coord(0, 0)), # center shift right 2 down 1
+        
+        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 1), Coord(2, 1)),  # L (L-shape)
+        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, 0), Coord(2, 0)), # center shift right 1
+        PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0)), # center shift right 1 down 1
+        PlaceAction(Coord(BOARD_N-2, BOARD_N-1), Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift right 1 down 2
+        
+        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 0), Coord(2, 0)),  # J (J-shape)
+        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, BOARD_N-1), Coord(2, BOARD_N-1)), # center shift right 1
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(BOARD_N-1, 1), Coord(0, 0), Coord(1, 0)), # center shift down 1
+        PlaceAction(Coord(BOARD_N-2, 0), Coord(BOARD_N-2, 1), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift down 2
     ]
     
     # rotate each tetronimo 4 times
@@ -22,6 +48,13 @@ def get_tetronimos() -> list[PlaceAction]:
     
     # add cube
     rotated_tetronimos.append(PlaceAction(Coord(0, 0), Coord(1, 0), Coord(0, 1), Coord(1, 1)))
+    # add cubes with different center
+    # shift down 1
+    rotated_tetronimos.append(PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(BOARD_N-1, 1), Coord(0, 1)))
+    # shift right 1
+    rotated_tetronimos.append(PlaceAction(Coord(0, BOARD_N-1), Coord(1, BOARD_N-1), Coord(0, 0), Coord(1, 0)))
+    # shift down 1 right 1
+    rotated_tetronimos.append(PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(0, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0)))
     return rotated_tetronimos
 
 def rotate(tetronimo: PlaceAction, times: int) -> PlaceAction:
