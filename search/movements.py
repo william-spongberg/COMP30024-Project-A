fdf3476bf69fa4acde8f0485cd330ea39348c04c
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-07 05:31:50 +0000

    sort adjacent coords by distance to goal

--- search/movements.py
+++ search/movements.py
@@ -1,5 +1,6 @@
 from .core import PlayerColor, Coord, PlaceAction, Direction
 from .tetronimos import get_moves
+from .heuristics import coord_distance_to_goal_line
 
 def get_valid_moves(board: dict[Coord, PlayerColor], tetronimos: list[PlaceAction], coord: Coord) -> list[PlaceAction]:
     """
@@ -11,8 +12,6 @@ def get_valid_moves(board: dict[Coord, PlayerColor], tetronimos: list[PlaceActio
     for move in get_moves(coord, tetronimos):
         if is_valid(board, move):
             valid_moves.append(move)
-
-    #print(valid_moves)
     return valid_moves
 
 def get_valid_adjacents_all_over_the_board(board: dict[Coord, PlayerColor], goal_line: list[Coord]) -> list[Coord]:
@@ -28,7 +27,7 @@ def get_valid_adjacents_all_over_the_board(board: dict[Coord, PlayerColor], goal
                 if not board.get(adjacent, None): # if adjacent is empty
                     valid_adjacents.append(adjacent)
     # rearrage list to order by distance to goal list
-    # valid_adjacents.sort(key=lambda x: heuristic_to_line(x, goal_line))
+    valid_adjacents.sort(key=lambda x: coord_distance_to_goal_line(goal_line, x))
     return valid_adjacents
 
 def is_valid(board: dict[Coord, PlayerColor], piece: PlaceAction) -> bool:
