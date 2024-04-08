c9825023368cccbdb934e96a28b5659c05f9a718
William Spongberg <wspongberg@student.unimelb.edu.au>
2024-04-08 03:55:00 +0000

    redid and fixed tetronimo piece generation

--- search/tetronimos.py
+++ search/tetronimos.py
@@ -8,37 +8,27 @@ def get_tetronimos() -> list[PlaceAction]:
     """
     tetronimos = [
         PlaceAction(Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(3, 0)),  # I (straight)
-        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0), Coord(2, 0)), # center shift down 1
-        PlaceAction(Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0)), # center shift down 2
-        # last case included in rotated tetronimos
-        # PlaceAction(Coord(BOARD_N-3, 0), Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift down 3
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0), Coord(2, 0)),
         
         PlaceAction(Coord(0, 0), Coord(1, 0), Coord(2, 0), Coord(1, 1)),  # T (T-shape)
-        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0), Coord(0, 1)), # center shift down 1
-        PlaceAction(Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(BOARD_N-1, 1)), # center shift down 2
-        PlaceAction(Coord(BOARD_N-1,BOARD_N-1), Coord(0,BOARD_N-1), Coord(1,BOARD_N-1), Coord(0, 0)), # center shift down 1 right 1
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0), Coord(0, 1)),
+        PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(0, BOARD_N-1), Coord(1, BOARD_N-1), Coord(0, 0)),
         
-        PlaceAction(Coord(0, 0), Coord(1, 0), Coord(1, 1), Coord(2, 1)),  # S (S-shape)
-        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(0, 1), Coord(1, 1)), # center shift down 1
-        # PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, 0)), # center shift down 1 right 1
-        # last case included in rotated tetronimos
-        # PlaceAction(Coord(BOARD_N-2, BOARD_N-1), Coord(BOARD_N-1, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift down 2 right 1
+        PlaceAction(Coord(0, 0), Coord(1, 0), Coord(1, BOARD_N-1), Coord(2, BOARD_N-1)),  # S (S-shape)
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(0, BOARD_N-1), Coord(1, BOARD_N-1)),
         
-        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 1), Coord(1, 2)), # Z (Z-shape)
-        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, 0), Coord(1, 1)), # center shift right 1
-        # PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(0, 1)), # center shift right 1 down 1
-        # last case included in rotated tetronimos
-        # PlaceAction(Coord(BOARD_N-1, BOARD_N-2), Coord(BOARD_N-1, BOARD_N-1), Coord(0, BOARD_N-1), Coord(0, 0)), # center shift right 2 down 1
+        PlaceAction(Coord(0, 0), Coord(1, 0), Coord(1, 1), Coord(2, 1)), # Z (Z-shape)
+        PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(0, 1), Coord(1, 1)),
         
-        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 1), Coord(2, 1)),  # L (L-shape)
-        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, 0), Coord(2, 0)), # center shift right 1
-        PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0), Coord(1, 0)), # center shift right 1 down 1
-        PlaceAction(Coord(BOARD_N-2, BOARD_N-1), Coord(BOARD_N-2, 0), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift right 1 down 2
+        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(0, 2), Coord(1, 2)),  # L (L-shape)
+        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(0, 1), Coord(1, 1)),
+        PlaceAction(Coord(0, BOARD_N-2), Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, 0)),
+        PlaceAction(Coord(BOARD_N-1, BOARD_N-2), Coord(BOARD_N-1, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0)),
         
-        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(1, 0), Coord(2, 0)),  # J (J-shape)
-        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(1, BOARD_N-1), Coord(2, BOARD_N-1)), # center shift right 1
-        PlaceAction(Coord(BOARD_N-1, 0), Coord(BOARD_N-1, 1), Coord(0, 0), Coord(1, 0)), # center shift down 1
-        PlaceAction(Coord(BOARD_N-2, 0), Coord(BOARD_N-2, 1), Coord(BOARD_N-1, 0), Coord(0, 0)), # center shift down 2
+        PlaceAction(Coord(0, 0), Coord(0, 1), Coord(0, 2), Coord(BOARD_N-1, 2)),  # J (J-shape)
+        PlaceAction(Coord(0, BOARD_N-1), Coord(0, 0), Coord(0, 1), Coord(BOARD_N-1, 1)),
+        PlaceAction(Coord(0, BOARD_N-2), Coord(0, BOARD_N-1), Coord(0, 0), Coord(BOARD_N-1, 0)),
+        PlaceAction(Coord(1, 2), Coord(1, 1), Coord(1, 0), Coord(0, 0)),
     ]
     
     # rotate each tetronimo 4 times
@@ -47,15 +37,11 @@ def get_tetronimos() -> list[PlaceAction]:
         for i in range(4):
             rotated_tetronimos.append(rotate(tetronimo, i))
     
-    # add cube
-    rotated_tetronimos.append(PlaceAction(Coord(0, 0), Coord(1, 0), Coord(0, 1), Coord(1, 1)))
-    # add cubes with different center
-    # shift down 1
+    rotated_tetronimos.append(PlaceAction(Coord(0, 0), Coord(1, 0), Coord(0, 1), Coord(1, 1)))  # O (square)
     rotated_tetronimos.append(PlaceAction(Coord(BOARD_N-1, 0), Coord(0, 0), Coord(BOARD_N-1, 1), Coord(0, 1)))
-    # shift right 1
     rotated_tetronimos.append(PlaceAction(Coord(0, BOARD_N-1), Coord(1, BOARD_N-1), Coord(0, 0), Coord(1, 0)))
-    # shift down 1 right 1
     rotated_tetronimos.append(PlaceAction(Coord(BOARD_N-1, BOARD_N-1), Coord(0, BOARD_N-1), Coord(BOARD_N-1, 0), Coord(0, 0)))
+    
     return rotated_tetronimos
 
 def rotate(tetronimo: PlaceAction, times: int) -> PlaceAction:
